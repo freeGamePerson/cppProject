bbb3b2f70ae091954e985e28d4c8c5bda25d5b79
WWWByby <18501604724@163.com>
2018-12-04 18:44:27 +0800

    sockt初见

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
 #include <Log.h>
+#include <arpa/inet.h>
 using namespace std;
 
-int main()
+
+
+
+
+int main(int argc,char * argv[])
 {
+	string selfIP = "127.0.0.1";
+	
+	int converIP = inet_pton()
+	
 	ERROR_LOG<std::string>("test---->>>");
 }
